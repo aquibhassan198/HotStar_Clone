1176f3157eb99ce098aa245f47a7dc61f1f0920d
aquibhassan198 <aquib.hassan198@gmail.com>
2024-04-27 23:41:52 +0530

    HotStar Clone

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Falcon And The Winter Soldier',
+        des: 'The Falcon and the Winter Soldier[a] is an American television miniseries created by Malcolm Spellman for the streaming service Disney+, based on Marvel Comics featuring the characters Sam Wilson / Falcon and Bucky Barnes / Winter Soldier. ',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'Loki',
+        des: 'The Mercurial villain Loki (Tom Hiddleston) resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'WandaVision',
+        des: 'The series is a blend of classic television and the Marvel Cinematic Universe in which Wanda Maximoff and Vision two super-powered beings living idealized suburban lives begin to suspect that everything is not as it seems.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'Raya And The Last Dragon',
+        des: 'Long ago, in the fantasy world of Kumandra, humans and dragons lived together in harmony. However, when sinister monsters known as the Druun threatened the land, the dragons sacrificed themselves to save humanity.',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'Luca',
+        des: 'Set in a beautiful seaside town on the Italian Riviera, Disney and Pixar Luca is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides.' ,
+        image: 'images/slider 5.PNG'
+    }
+]
